a5e0a6f9fc1516638f60e13b7a6065860c238903
Laptop <laptop@disroot.org>
2023-10-20 19:26:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-function showTab(tabId) {
+function showTab(element, tabId) {
   // Hide all tab content elements
-  const tabPanes = document.querySelectorAll(".tab-pane");
-  tabPanes.forEach(tabPane => tabPane.classList.remove("selected"));
+  document.getElementsByClassName("tab-pane").forEach(tabPane => tabPane.classList.remove("selected"));
 
   // Remove 'active' class from all tab buttons
-  const tabButtons = document.querySelectorAll(".tab-button");
-  tabButtons.forEach(tabButton => tabButton.classList.remove("selected"));
+  document.getElementsByClassName("tab-button").forEach(tabButton => tabButton.classList.remove("selected"));
 
   // Show the selected tab content
-  const selectedTab = document.getElementById(tabId);
-  selectedTab.classList.add("selected");
+  document.getElementById(tabId).classList.add("selected");
 
   // Add 'active-button' class to the clicked tab button
-  const clickedButton = document.querySelector(`[onclick="showTab('${tabId}')"]`);
-  clickedButton.classList.add("selected");
+  element.classList.add("selected");
 }
 
 // Tab to show on load
-window.onload = () => showTab("tab1");
+window.onload = () => showTab(document.getElementsByClassName("tab-button")[0], "tab1"); // this should use the first element with the class tab-button present in dom
